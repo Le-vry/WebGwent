c4f5818b797301dddb2dab9b9630733c6bce0b35
Elliot Lidman <elllid@elevad.umea.se>
2025-04-10 08:19:49 +0200

    message

---
+++ docs/_app/immutable/chunks/D7uBbl-w.js
@@ -0,0 +1,1 @@
+import{w as o}from"./D9iaEa-w.js";const r=(window==null?void 0:window.localStorage.getItem("players"))??"",s=o(r);s.subscribe(e=>{window==null||window.localStorage.setItem("players",e)});export{s as p};
